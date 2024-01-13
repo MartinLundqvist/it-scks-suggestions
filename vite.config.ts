1593331437129a5f793b39071cce1d953927ff21
MartinLundqvist <iphonelynden@gmail.com>
2024-01-13 12:02:25 +0100

    Chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          firebase: [
+            'react-firebase-hooks/auth',
+            'react-firebase-hooks/firestore',
+            'firebase/auth',
+            'firebase/app',
+          ],
+          firebaseFirestore: ['firebase/firestore'],
+          other: ['nanoid', 'bad-words', 'styled-components'],
+        },
+      },
+    },
+  },
+});
